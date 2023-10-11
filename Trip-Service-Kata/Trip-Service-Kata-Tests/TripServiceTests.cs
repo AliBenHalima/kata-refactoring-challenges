8ac9ca024a8e1d63646df878496b3b4f5bc094b4
Ben Halima Ali <alibenhalima60@gmail.com>
2023-10-11 15:28:07 +0100

    wip: adding-tests-to-existing-code.

---
+++ Trip-Service-Kata/Trip-Service-Kata-Tests/TripServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
+using TripServiceKata.User;
+
+namespace Trip_Service_Kata_Tests
+{
+    public class TripServiceTests
+    {
+        [Fact]
+        public void GetTripsByUser_Should_Return_List_Of_Trips()
+        {
+            //Arrange
+            var sut = new TripService();
+            var user = new User();
+            //Act
+            Action result = () => sut.GetTripsByUser(user);
+            //Assert
+            Assert.Throws<DependendClassCallDuringUnitTestException>(result);
+
+        }
+    }
+}
